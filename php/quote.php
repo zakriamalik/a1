48a9a33268a6622624232b48df4c2172cb53c505
Zakria Malik <zakria_malik@hotmail.com>
2017-02-09 19:48:31 -0600

    updated text on self, reduced img size, max var added to php code

--- php/quote.php
+++ php/quote.php
@@ -1,11 +1,16 @@
 <?php
 function getRandomQuote() {
-
-	$quotes = ["Love for all, malice for none",
-                 "Do your worst, for I shall do mine, then let fate & time decide between us",
-                 "Nothing is impossible, even the the word IMPOSSIBLE itself suggests.. I-M-POSSIBLE"];
-
-    $x=rand(0,2);
-
-    return $quotes[$x];
+	//array declaration and load up with data
+	$quotes = ["Love for all, malice for none (Lincoln)",
+              "Do your worst, for I shall do mine, then let fate & time decide between us (Unknown)",
+              "Nothing is impossible, even the the word IMPOSSIBLE itself suggests.. I-M-POSSIBLE (Unknown)",
+							"We are what we repeatedly do; excellence, then, is not an act but a habit (Aristotle)",
+							"You must be the change you wish to see in the world (Ghandi)",
+							"Try not to become a man of success, but rather try to become a man of value (Einstein)"];
+	//variable with a max number based on objects in the array
+	$arraySize = Count($quotes)-1;
+	//variable with the assignment of random number from zero to a max based on array size
+  $x=rand(0,$arraySize);
+	//return the array object based on the random number generated from above statement
+  return $quotes[$x];
 }
